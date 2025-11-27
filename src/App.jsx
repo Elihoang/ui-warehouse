a73352ad21dc78f531d061a9545c27a4fa5b237f
Lê Duy Hoàng <129831728+Elihoang@users.noreply.github.com>
2025-11-27 16:42:26 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return <div className="text-3xl font-bold underline">Hello Vite + React!
+  <Button>clicl me</Button></div>
+}
